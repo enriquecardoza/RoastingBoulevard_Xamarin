93577e27f491b5b13267189e7b4bd19d20cced64
enrique <enricard.developer@gmail.com>
2021-03-24 19:14:56 +0100

    Vista crear usuario

---
+++ RoastingBoulevard/Views/ToDo/CreateEditAddress.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RoastingBoulevard.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreateEditAddress : ContentPage
+    {
+        public CreateEditAddress()
+        {
+            InitializeComponent();
+        }
+    }
+}
